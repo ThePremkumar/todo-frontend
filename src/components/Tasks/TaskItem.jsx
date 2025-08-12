d5e17420962754185877f1b23e3023df5e5fa8e0
ThePremkumar <premkumr2462004@gmail.com>
2025-08-12 21:02:55 +0530

    update the status text color

--- src/components/Tasks/TaskItem.jsx
+++ src/components/Tasks/TaskItem.jsx
@@ -14,9 +14,9 @@ export const TaskItem = ({ task, onEdit }) => {
   const [isToggling, setIsToggling] = useState(false);
 
   const priorityClasses = {
-    low: 'bg-success bg-opacity-10 text-success fw-bold',
-    medium: 'bg-warning bg-opacity-10 text-warning fw-bold',
-    high: 'bg-danger bg-opacity-10 text-danger fw-bold',
+    low: 'bg-success bg-opacity-10 text-black fw-bold',
+    medium: 'bg-warning bg-opacity-10 text-black fw-bold',
+    high: 'bg-danger bg-opacity-10 text-black fw-bold',
   };
 
   const formatDate = (dateString) => {
